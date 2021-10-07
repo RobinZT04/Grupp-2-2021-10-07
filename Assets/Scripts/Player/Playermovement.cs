35a90c0ce7dc1652e9b7573c218c6278ac274acf
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2021-10-07 15:21:23 +0200

    Började med Player Movement

--- Assets/Scripts/Player/Playermovement.cs
+++ Assets/Scripts/Player/Playermovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D body; //referens till spelaren Rigidbody2D - Robin
+
+    [Range(1000f, 8000f)] //gör en "slider" i inspectorn vilket gör det lättare att justera fart till spelaren - Robin
+    public float speed; //variabel till speed - Robin
+
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float vert = Input.GetAxis("Vertical"); //gör en float variabel som heter vert och ger den värdet av input.getaxis av "Vertical" - Robin
+        float horiz = Input.GetAxis("Horizontal"); //gör en float variabel som heter horiz och ger den värdet av input.getaxis av "Horizontal" - Robin
+
+        body.velocity = new Vector2(horiz * speed * Time.deltaTime, vert * speed * Time.deltaTime); //sätter body.velocity till horizons velocity samt verticals velocity - Robin
     }
 }
