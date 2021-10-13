70313ab7297472041bf18afe1bea01c83bd2c8ab
Lidok123 <79506296+Lidok123@users.noreply.github.com>
2021-10-13 10:13:15 +0200

    Grundfunktioner
    En början på Enemy from top

--- Assets/Scripts/Enemies(from top)/EnemyTopMove.cs
+++ Assets/Scripts/Enemies(from top)/EnemyTopMove.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemiesFromTop : MonoBehaviour
+public class EnemyTopMove : MonoBehaviour
 {
-    // Referens till enemyn -Henry
-    public GameObject enemytop;
-
     // Referens till rigidbodyn -Henry
     Rigidbody2D rb;
-    
+
     // Referens till enemyns snabbhet -Henry
     private int enemyspeed = -3;
 
@@ -19,11 +16,6 @@ public class EnemiesFromTop : MonoBehaviour
         // Hämtar componenten och rör enemyn nedåt -Henry
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, enemyspeed);
-
-        for (int i = 0; i < 10; i++)
-        {
-            //Instantiate(enemytop, new Vector3(Random.Range(0, 5.5), transform.position.y, Quaternion.identity));
-        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Enemies(from top)/EnemyTopSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTopSpawner : MonoBehaviour
+{
+    // Referens till enemyn -Henry
+    public GameObject enemytop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Upprepar koden 3 ggr -Henry
+        for (int i = 0; i < 3; i++)
+        {
+            // Klonar enemytop till ett random ställe i x-axeln -Henry
+            Instantiate(enemytop, new Vector3(Random.Range(-12.5f, 12.5f), Random.Range(11, 20), 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
