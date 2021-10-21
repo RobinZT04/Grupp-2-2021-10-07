5e37433f25f17bad575e5394750a1fe53d1c53ad
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2021-10-21 15:22:31 +0200

    Håller på med animationer

--- Assets/Scripts/Player/Playermovement.cs
+++ Assets/Scripts/Player/Playermovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour, IWalk
 {
+    public Animator playeranim;
     public Rigidbody2D body; //referens till spelaren Rigidbody2D - Robin
 
     [Range(200f, 1500f)] //gör en "slider" i inspectorn vilket gör det lättare att justera fart till spelaren - Robin
@@ -16,6 +17,9 @@ public class Playermovement : MonoBehaviour, IWalk
         float horiz = Input.GetAxis("Horizontal"); //gör en float variabel som heter horiz och ger den värdet av input.getaxis av "Horizontal" - Robin
 
         body.velocity = new Vector2(horiz * speed * Time.deltaTime, vert * speed * Time.deltaTime); //sätter body.velocity till horizons velocity samt verticals velocity - Robin
-
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+        {
+            playeranim.SetBool("Walking", true);
+        }
     }
 }
