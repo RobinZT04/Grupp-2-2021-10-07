3dc9e495c07730492e24cdd631ada4954079d8d1
Elanor <elanor.berg@elev.ga.lbs.se>
2021-10-22 14:18:05 +0200

    Fortsättning på att göra vägen

--- Assets/Scripts/Side enemies/barrier.cs
+++ Assets/Scripts/Side enemies/barrier.cs
@@ -4,25 +4,40 @@ using UnityEngine;
 
 public class barrier : MonoBehaviour
 {
-    public Collider2D väg;
+    public float hp;
+
+    public Animator table;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        if (hp == 3)
+        {
+            table.SetBool("hp1", true);
+        }
+        if (hp ==2) 
+        {
+            table.SetBool("hp2", true);
+        }
+        if (hp == 1)
+        {
+           
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.tag == "Enemy")
         {
-           
+            hp -= 1;
+            
         }
     }
 }
