d68ad93985da0059ffa76f934d0052bbdb32a27d
leaguecomapany <74959602+leaguecomapany@users.noreply.github.com>
2021-10-24 23:02:32 +0200

    just restarted

--- Assets/Scripts/Boss 1/BossActivation.cs
+++ Assets/Scripts/Boss 1/BossActivation.cs
@@ -11,15 +11,9 @@ public class BossActivation : MonoBehaviour
         if(collision.CompareTag("Player")) // if the collision happen with something with the tag Player 
         {
             BossUI.instance.BossActivator();
-            StartCoroutine(WaitForBoss());
+            
         }
     }
-    IEnumerator WaitForBoss()
-    {
-        var currentSpeed = PlayerController.instance.speed; // we will save our speed that we have on currentSpeed
-        PlayerController.instance.speed = 0; // we will change it to 0 
-        yield return new WaitForSeconds(3); // and after 3 seconds 
-        PlayerController.instance.speed = currentSpeed; // the 0 will go back to the ccurrentSpeed
-    }
+  
 
 }
