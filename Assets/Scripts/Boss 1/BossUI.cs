6b7198a53fcb1e2936c6fc86590975f48792e8b1
leaguecomapany <74959602+leaguecomapany@users.noreply.github.com>
2021-10-26 10:00:24 +0200

    changed the script for my attack

--- Assets/Scripts/Boss 1/BossUI.cs
+++ Assets/Scripts/Boss 1/BossUI.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class BossUI : MonoBehaviour
 {
 
-    public GameObject bossPanel;
-    public GameObject tablewalls;
+   
+    
 
     public static BossUI instance;
 
@@ -19,17 +19,5 @@ public class BossUI : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        bossPanel.SetActive(false);
-        tablewalls.SetActive(false);
-    }
-
-    // Update is called once per frame
-    public void BossActivator()
-    {
-        bossPanel.SetActive(true);
-        tablewalls.SetActive(true);
-    }
+   
 }
