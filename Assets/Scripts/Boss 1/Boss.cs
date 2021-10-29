7935acafa0453616dd0fca551f938f8bc71118e1
leaguecomapany <74959602+leaguecomapany@users.noreply.github.com>
2021-10-29 05:01:33 +0200

    Movement change

--- Assets/Scripts/Boss 1/Boss.cs
+++ Assets/Scripts/Boss 1/Boss.cs
@@ -11,8 +11,49 @@ public class Boss : MonoBehaviour
     public float knockbackForceY; // 
     public float damageToGive; // the damage the enemy will do to the player
 
+	public Rigidbody2D boss; //reference to itself
+
+	public float moveSpeed = 15.0f; //default move speed of the enemy
+
+	public bool changeDirection = false; //by default set the bool to false
+
+	// Use this for initialization
+	void Start()
+	{
+		boss = this.gameObject.GetComponent<Rigidbody2D>(); //make the connection to the reference
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		moveEnemy();
+	}
+
+	public void moveEnemy()
+	{
+
+		if (changeDirection == true)
+		{
+			boss.velocity = new Vector2(1, 0) * -1 * moveSpeed; //get the enemy to move left
+		}
+		else if (changeDirection == false)
+		{
+			boss.velocity = new Vector2(1, 0) * moveSpeed; //get the enemy to move right
+		}
+	}
 
 
-
-  
-}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.name == "RightWall")
+		{
+			Debug.Log("Hit the right wall");
+			changeDirection = true;
+		}
+		if (col.gameObject.name == "LeftWall")
+		{
+			Debug.Log("Hit the left wall");
+			changeDirection = false;
+		}
+	}
+} 
